c53eb3b36013673f746451b80294fbe2c54b8c21
wndr <ca223871@gmail.com>
2026-03-14 22:14:25 -0600

    feat: add register and email verification endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,25 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { VerifiyOtpDto } from './dto/verify-otp.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @Post('verify-email')
+  verifiyEmail(@Body() verificationDto: VerifiyOtpDto) {
+    return this.authService.verifiyEmail(verificationDto);
+  }
+
+  @Post('resend-otp')
+  resendOtpEmail(@Body('email') email: string) {
+    return this.resendOtpEmail(email);
+  }
 
 }
